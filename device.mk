010c50dd4395ff99fe3fe2a9f5b966626ca0bac3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-02 22:57:53 -0200

    potter: Build Camera2 for now

--- device.mk
+++ device.mk
@@ -118,7 +118,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    GoogleCamera \
+    Camera2 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
