b8f02f60f94030c21b3ee7b0b86b2067fc38f6d1
iykequame <izaqkull@yahoo.com>
2018-02-06 11:39:29 +0000

    Revert "dex ftw"
    This reverts commit 62cf1913f981f509d1543bf9f201fb0a7f5bfd0a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,8 @@ DEVICE_PATH := device/motorola/potter
 BOARD_VENDOR := motorola-qcom
 
 # Dexpreopt
-WITH_DEXPREOPT := true
+WITH_DEXPREOPT ?= true
+DONT_DEXPREOPT_PREBUILTS := true
 
 # AIDs and CAPS
 TARGET_FS_CONFIG_GEN := \
